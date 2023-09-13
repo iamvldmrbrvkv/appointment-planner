75156a905c1b7d7aed773142116d13154ef382e0
Vladimir Borovikov <91381325+iamvldmrbrvkv@users.noreply.github.com>
2023-09-13 10:42:59 +0300

    feat: add states, add contact info, add duplicates check in useEffect (#2)

--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -8,6 +8,10 @@ export const ContactsPage = ({ contacts, addContact }) => {
   Define state variables for 
   contact info and duplicate check
   */
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+  const [duplicate, setDuplicate] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -15,21 +19,41 @@ export const ContactsPage = ({ contacts, addContact }) => {
     Add contact info and clear data
     if the contact name is not a duplicate
     */
+    if (!duplicate) {
+      addContact({
+        name: name,
+        phone: phone,
+        email: email
+      });
+      setName('');
+      setPhone('');
+      setEmail('');
+    }
   };
 
   /*
   Using hooks, check for contact name in the 
   contacts array variable in props
   */
-
+  useEffect(() => {
+    contacts.forEach(contact => {
+      if (contact.name === name) {
+        setDuplicate(true);
+        alert('The name is a duplicate');
+      }
+    })
+  });
+  
   return (
     <div>
       <section>
-        <h2>Add Contact</h2> 
+        <h2>Add Contact</h2>
+        <ContactForm name={name} phone={phone} email={email} handleSubmit={handleSubmit} />
       </section>
       <hr />
       <section>
         <h2>Contacts</h2>
+        <TileList contacts={contacts} />
       </section>
     </div>
   );
