6c44e4a65aa5d8c6d8ba51d093924f03cd1d2222
Vladimir Borovikov <91381325+iamvldmrbrvkv@users.noreply.github.com>
2023-09-14 22:32:28 +0300

    feat: add iteratively render Tile components

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Tile } from "../tile/Tile";
 
-export const TileList = () => {
+export const TileList = ({ contacts }) => {
   return (
     <div>
-      
+      {contacts.map((tile, i) => {
+        const { name, ...description } = tile;
+        <Tile name={name} description={description} key={i} />
+      })}
     </div>
   );
 };
