6cc97e97e485022300d47a6986aade1254cf59f0
Vladimir Borovikov <91381325+iamvldmrbrvkv@users.noreply.github.com>
2023-09-15 17:24:04 +0300

    feat: add states; add handleSubmit; add render components

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -3,28 +3,53 @@ import React, { useState } from "react";
 import { AppointmentForm } from "../../components/appointmentForm/AppointmentForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const AppointmentsPage = ({ appointments, addAppointment }) => {
+export const AppointmentsPage = ({ appointments, addAppointment, contacts }) => {
   /*
   Define state variables for 
   appointment info
   */
+  const [name, setName] = useState('');
+  const [contact, setContact] = useState('');
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
     /*
     Add contact info and clear data  
     */
-   
+    addAppointment({
+      name: name,
+      contact: contact,
+      date: date,
+      time: time
+    })
+    setName('');
+    setContact('');
+    setDate('');
+    setTime('');
   };
 
   return (
     <div>
       <section>
         <h2>Add Appointment</h2>
+        <AppointmentForm
+          contacts={contacts}
+          name={name}
+          setName={setName}
+          contact={contact}
+          setContact={setContact}
+          date={date}
+          setDate={setDate}
+          time={setTime}
+          handleSubmit={handleSubmit}
+        />
       </section>
       <hr />
       <section>
         <h2>Appointments</h2>
+        <TileList appointments={appointments} />
       </section>
     </div>
   );
