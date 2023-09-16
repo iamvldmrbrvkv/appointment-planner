7f9eba32ebb270795854255ee51c87a39cd60929
Vladimir Borovikov <91381325+iamvldmrbrvkv@users.noreply.github.com>
2023-09-16 16:30:15 +0300

    feat: add render a <select> element; add iteratively render a <option> elements

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-export const ContactPicker = () => {
+export const ContactPicker = ({ name, value, onChange, contacts }) => {
   return (
-    <></>
+    <>
+      <select name={name} value={value} onChange={onChange}>
+        <option value="">No Contact Selected</option>
+        {contacts.map((contact, i) => (
+          <option key={i}>{contact.name}</option>
+        ))}
+      </select>
+    </>
   );
 };
