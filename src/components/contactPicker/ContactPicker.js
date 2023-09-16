d1e7125afcf60798c6c2382fed01588796ba2702
Vladimir Borovikov <91381325+iamvldmrbrvkv@users.noreply.github.com>
2023-09-16 21:21:30 +0300

    feat: add render a <select> element; add iteratively render a <option… (#8)
    * feat: add render a <select> element; add iteratively render a <option> elements
    
    * fix: props
    
    * fix: add a key to default <option> element

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,18 @@
 import React from "react";
 
-export const ContactPicker = () => {
+export const ContactPicker = ({ name, value, onChange, contacts }) => {
   return (
-    <></>
+    <>
+      <select name={name} value={value} onChange={onChange}>
+        <option value={""} key={-1}>
+          No Contact Selected
+        </option>
+        {contacts.map(contact => (
+          <option value={contact.name} key={contact.name}>
+            {contact.name}
+          </option>
+        ))}
+      </select>
+    </>
   );
 };
