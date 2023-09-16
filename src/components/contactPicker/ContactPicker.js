d5a0ea2dbde72bf288f65db9ab8192a487e5bfb5
Vladimir Borovikov <91381325+iamvldmrbrvkv@users.noreply.github.com>
2023-09-16 17:28:32 +0300

    fix: add a key to default <option> element

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -4,7 +4,9 @@ export const ContactPicker = ({ name, value, onChange, contacts }) => {
   return (
     <>
       <select name={name} value={value} onChange={onChange}>
-        <option value={""}>No Contact Selected</option>
+        <option value={""} key={-1}>
+          No Contact Selected
+        </option>
         {contacts.map(contact => (
           <option value={contact.name} key={contact.name}>
             {contact.name}
