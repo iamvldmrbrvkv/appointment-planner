cf00e3b11f005972e6e0c842d5ccc2d35930781e
Vladimir Borovikov <91381325+iamvldmrbrvkv@users.noreply.github.com>
2023-09-16 17:11:03 +0300

    fix: props

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -4,9 +4,11 @@ export const ContactPicker = ({ name, value, onChange, contacts }) => {
   return (
     <>
       <select name={name} value={value} onChange={onChange}>
-        <option value="">No Contact Selected</option>
-        {contacts.map((contact, i) => (
-          <option key={i}>{contact.name}</option>
+        <option value={""}>No Contact Selected</option>
+        {contacts.map(contact => (
+          <option value={contact.name} key={contact.name}>
+            {contact.name}
+          </option>
         ))}
       </select>
     </>
